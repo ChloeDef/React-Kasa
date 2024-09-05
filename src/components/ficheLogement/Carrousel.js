c4627679d873d6b9b14a50ae1f65431ff8a10647
Chloe Defin <chloe.defin@gmail.com>
2024-09-05 14:14:41 +0200

    udpdate Carrousel

--- src/components/ficheLogement/Carrousel.js
+++ src/components/ficheLogement/Carrousel.js
@@ -5,11 +5,11 @@ function Carrousel({ slides }) {
   const [current, setCurrent] = useState(0); //je définie l'index du premier slide à 0
   const length = slides.length; //longueur du tableau de slides
 
-  /**Function pour l'image precedente */
+  /* Function pour l'image precedente */
   const nextImage = () => {
     setCurrent(current === length - 1 ? 0 : current + 1); // on repart au premier slide quand on arrive au dernier
   };
-  /**Function pour l'image suivante */
+  /* Function pour l'image suivante */
   const prevImage = () => {
     setCurrent(current === 0 ? length - 1 : current - 1); // on repart au dernier slide quand on est au premier
   };
